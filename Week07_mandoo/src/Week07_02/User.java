4b5954f1f79dbf0eb1708a5e38c9e3bf8be90390
suhyun__ <thebbbb1206@gmail.com>
2023-10-26 16:51:36 +0900

    7주차 과제 최종 push

---
+++ Week07_mandoo/src/Week07_02/User.java
@@ -0,0 +1,31 @@
+package Week07_02;
+
+public class User {
+	private int num;
+	private String name, tel, email;
+	
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
--- Week07_mandoo/src/Week07_02/UserInform.java
+++ Week07_mandoo/src/Week07_02/UserInform.java
@@ -1,34 +1,53 @@
 package Week07_02;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class UserInform {
 	public static void main(String[] args) throws IOException{
 		int num2;
 		String search;
-		String num, name, tel, email;
 		//Scanner scan= null;
-		PrintWriter in = new PrintWriter(new FileWriter("user.txt"));
+		PrintWriter outputStream = new PrintWriter(new FileWriter("user.txt"));
 		Scanner s = new Scanner(System.in);
+		List<User> userList = new ArrayList<User>();
 		
 		while(true) {
-			System.out.print("번호 : ");
-			num = s.next();
-			System.out.print("이름 : ");
-			name = s.next();
-			System.out.print("전화번호 : ");
-			tel = s.next();
-			System.out.print("이메일 : ");
-			email = s.next();
-			in.println(num + "," + name + "," + tel + "," + email);
+			User newUser = new User();
 			
-			System.out.println("입력을 끝났으면 1, 계속하려면 0: ");
+			System.out.print("번호 : ");
+			newUser.setNum(s.nextInt());
+			
+			System.out.print("이름 : ");
+			newUser.setName(s.next());
+			
+			System.out.print("전화번호 : ");
+			newUser.setTel(s.next());
+			
+			System.out.print("이메일 : ");
+			newUser.setEmail(s.next());
+			
+			userList.add(newUser);
+			
+			outputStream.println(newUser.getNum() + "," + newUser.getName() + "," + newUser.getTel() + "," + newUser.getEmail());
+			outputStream.flush();
+			
+			System.out.println("입력이 끝났으면 0, 계속하려면 1: ");
 			num2 = s.nextInt();
-			if(num2 == 1) {
-				s.close();
-				in.close();
+			if(num2 == 0) {
 				break;
+			}
+		}
+		
+		System.out.println("모든 사용자 정보 입력이 완료 되었습니다.");
+		System.out.print("검색하고 싶은 사용자 번호를 입력 : ");
+		int userNum = s.nextInt();
+		
+		for(User userInfo : userList) {
+			if(userInfo.getNum() == userNum) {
+				System.out.println("해당 사용자 전화번호 : " + userInfo.getTel());
 			}
 		}
 	}
